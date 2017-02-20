cd50bfd33040786c7218f37703a7c05320fb91ea
flxmdy <flxmdy@gmail.com>
2017-02-20 00:16:20 -0500

    Added initial files.

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install ansible
+
+echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+apt-get update
+apt-get -y install ansible
+
+# Run playbook
+
+ansible-playbook -k main.yml
